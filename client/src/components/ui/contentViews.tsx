1b4150f1fc546b44d77da2c6a5e589844d036fbe
LazzeriLuca <luca.lazzeri@additive.eu>
2024-11-09 13:03:01 +0100

    fix scroll behaviour and contentViews

--- client/src/components/ui/contentViews.tsx
+++ client/src/components/ui/contentViews.tsx
@@ -1,11 +1,13 @@
 const ContentViews = ({NavBar, Content}) => {
   return (
-      <div className="h-full w-full overflow-hidden max-h-lvh flex-col relative flex">
-        <div className='absolute top-0 right-0 left-0 h-20 w-full flex flex-ro items-center'>
-          {NavBar}
-        </div>
-        <div className='w-full m-auto overflow-auto flex py-3 flex flex-col flex-grow'>{Content}</div>
+    <div className='navContainer h-full w-full overflow-hidden max-h-lvh flex-col relative flex'>
+      <div className='absolute top-0 right-0 left-0 h-20 w-full flex flex-ro items-center'>
+        {NavBar}
       </div>
+      <div className='mainContainer w-full m-auto overflow-auto flex py-3 pt-32 flex flex-col flex-grow'>
+        {Content}
+      </div>
+    </div>
   );
 };
 

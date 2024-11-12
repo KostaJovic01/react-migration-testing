6d72de121488a0eb170158a2a61e925de453ee19
LazzeriLuca <luca.lazzeri@additive.eu>
2024-11-12 11:41:38 +0100

    Add callback

--- client/src/components/ui/buttons/addButton.tsx
+++ client/src/components/ui/buttons/addButton.tsx
@@ -3,14 +3,16 @@ import {Button} from '../button';
 import {Add} from '@/components/icons/Add';
 
 export interface IAddButtonProps {
-  className?: string; // Add an optional className prop
+  className?: string; 
+  onClick?: () => void; 
 }
 
 export default class AddButton extends React.Component<IAddButtonProps> {
   public render() {
-    const {className} = this.props;
+    const {className, onClick} = this.props;
     return (
       <Button
+        onClick={onClick}
         className={`z-20 min-w-0 w-14 h-14 hover:w-12 hover:h-12 hover:m-1 p-0 rounded-full bg-clip-padding overflow-hidden bg-uiColorPrimary ${className}`}>
         <Add color='white' />
       </Button>
